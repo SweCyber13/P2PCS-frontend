4f1f6e767b86b1ddb7fb26f7fb016b78d9bf644f
ila <ilarya9531@gmail.com>
2019-05-03 13:56:18 +0200

    agguinta cardview non funzionante

---
+++ P2PCS/app/src/main/java/com/example/p2pcs_cyber/Model.kt
@@ -0,0 +1,34 @@
+package com.example.p2pcs_cyber
+
+data class Hobby(var title: String)
+
+object Suplier {
+    val hobbies = listOf<Hobby>(Hobby("Swimming"),
+                                Hobby("Reading"),
+                                Hobby("Walking"),
+                                Hobby("Sleeping"),
+                                Hobby("Gaming"),
+                                Hobby("Programming"),
+                                Hobby("Talking"),
+                                Hobby("Swimming"),
+                                Hobby("Reading"),
+                                Hobby("Walking"),
+                                Hobby("Sleeping"),
+                                Hobby("Gaming"),
+                                Hobby("Programming"),
+                                Hobby("Talking"),
+                                Hobby("Swimming"),
+                                Hobby("Reading"),
+                                Hobby("Walking"),
+                                Hobby("Sleeping"),
+                                Hobby("Gaming"),
+                                Hobby("Programming"),
+                                Hobby("Talking"),
+                                Hobby("Swimming"),
+                                Hobby("Reading"),
+                                Hobby("Walking"),
+                                Hobby("Sleeping"),
+                                Hobby("Gaming"),
+                                Hobby("Programming"),
+                                Hobby("Talking"))
+}
