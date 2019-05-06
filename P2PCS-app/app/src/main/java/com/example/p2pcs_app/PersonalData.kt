ce744ec07385e8a095b4d40845770cd179414325
Msqueri <squeri1997@hotmail.it>
2019-05-06 18:12:19 +0200

    popuptry

--- P2PCS-app/app/src/main/java/com/example/p2pcs_app/PersonalData.kt
+++ P2PCS-app/app/src/main/java/com/example/p2pcs_app/PersonalData.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,10 +13,14 @@ class PersonalData : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
-        val button1= findViewById<Button>(R.id.button1)
-        button1.setOnClickListener{
-            Toast.makeText(this, "You clicked me.", Toast.LENGTH_SHORT).show()}
+        val button = findViewById<ImageView>(R.id.provapopup)
+        val fragment_manager = supportFragmentManager
+        val editnamedialog= EditNameFragment()
+        button.setOnClickListener{
+            editnamedialog.show(fragment_manager,"changename")
+        }
     }
+
 
 
 
