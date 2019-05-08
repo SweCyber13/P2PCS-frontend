62131b9abfe6acad020d3034345c98ea1e14386a
ila <ilarya9531@gmail.com>
2019-05-08 15:47:41 +0200

    aggiunto parco macchine e pagina con info macchina

--- P2PCS-app/app/src/main/java/com/example/p2pcs_app/CarsFragment.kt
+++ P2PCS-app/app/src/main/java/com/example/p2pcs_app/CarsFragment.kt
@@ -1,16 +1,29 @@
 package com.example.p2pcs_app
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class CarsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_cars, null)
+        val view = inflater.inflate(R.layout.fragment_cars, null)
+        val button = view.findViewById<Button>(R.id.InfoCar)
+        button.setOnClickListener{
+            val intent= Intent(requireContext(), EditCar::class.java)
+            startActivity(intent)
+        }
+
+
+        return view
     }
+
+
+
 }
--- P2PCS-app/app/src/main/java/com/example/p2pcs_app/ExploreFragment.kt
+++ P2PCS-app/app/src/main/java/com/example/p2pcs_app/ExploreFragment.kt
@@ -18,41 +18,7 @@ import org.json.JSONObject
 
 
 class ExploreFragment : Fragment() {
-    fun getUsers() {
-        // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-        val url: String = "http://ec2-18-206-124-50.compute-1.amazonaws.com/volleytest.php"
 
-        // Request a string response from the provided URL.
-        //object property needed to override getparams
-        val stringReq = object: StringRequest(
-            Request.Method.POST, url,
-            Response.Listener<String> { response ->
-
-                val strResp = response.toString()
-                val jsonArray: JSONArray = JSONArray(strResp)
-                //val jsonArray: JSONArray = jsonObj.getJSONArray() //devo mettere un nome in qualche modo all'array su php
-                var str_user: String = ""
-                for (i in 0 until jsonArray.length()) {
-                    val jsonInner: JSONObject = jsonArray.getJSONObject(i)
-                    str_user = str_user + "\n" + jsonInner.get("Targa")
-                }
-                textView!!.text = "response : $str_user "
-            },
-            Response.ErrorListener {
-                textView!!.text = it.toString()
-            })
-        //need to override getparams to get the post request
-        {
-            override fun getParams() : Map<String,String> {
-                val params = HashMap<String, String>()
-                params.put("USER","admin")
-                return params
-            }
-        }
-
-        queue.add(stringReq)
-    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_explore, null)
 
