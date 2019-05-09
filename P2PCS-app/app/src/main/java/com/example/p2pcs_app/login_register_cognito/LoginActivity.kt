cd717b106ec4a4e0d50bf681b8731ce538ca2be8
Msqueri <squeri1997@hotmail.it>
2019-05-09 16:11:47 +0200

    cognito dependencies and register package

---
+++ P2PCS-app/app/src/main/java/com/example/p2pcs_app/login_register_cognito/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.p2pcs_app.login_register_cognito
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.p2pcs_app.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+    }
+
+
+
+
+
+}
