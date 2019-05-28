e5cff79eff1089fdb219bd5b2592719a893134c9
Elena Pontecchiani <ele.pontecchiani@gmail.com>
2019-05-28 14:50:01 +0200

    home

---
+++ P2PCS-app/app/src/main/java/com/example/p2pcs_app/HomeActivity.kt
@@ -0,0 +1,25 @@
+package com.example.p2pcs_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+
+
+class HomeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+
+    }
+
+
+}
