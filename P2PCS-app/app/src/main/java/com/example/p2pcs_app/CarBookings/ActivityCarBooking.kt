76fed501829c65ef46a04db385d495e3203e8024
ila <ilarya9531@gmail.com>
2019-06-10 13:52:06 +0200

    ..

--- P2PCS-app/app/src/main/java/com/example/p2pcs_app/CarBookings/ActivityCarBooking.kt
+++ P2PCS-app/app/src/main/java/com/example/p2pcs_app/CarBookings/ActivityCarBooking.kt
@@ -3,19 +3,14 @@ package com.example.p2pcs_app.CarBookings
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.Gravity
 import com.example.p2pcs_app.R
-
-import android.support.v4.view.PagerTabStrip
-
-
 
 
 class ActivityCarBooking : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_car_page)
+        setContentView(R.layout.activity_car_booking)
 
         val viewPager = findViewById<ViewPager>(R.id.viewPager)
 
