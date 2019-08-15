10592bc58291915b94fe7a3d9e07e216b7b074d0
Elena Pontecchiani <ele.pontecchiani@gmail.com>
2019-08-15 19:33:37 +0200

    ruota una sola volta

--- P2PCS-app/app/src/main/java/com/example/p2pcs_app/Home/FragmentHome.kt
+++ P2PCS-app/app/src/main/java/com/example/p2pcs_app/Home/FragmentHome.kt
@@ -22,67 +22,235 @@ import kotlinx.android.synthetic.main.fragment_home.view.*
 
 
 class HomeFragment : Fragment() {
+    class FirstAccess {
+        companion object {
+            var fa = true
+            fun setFalse() { fa=false}
+        }
+    }
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_home, null)
-
-
-        val button1 = view.findViewById<Button>(R.id.find_car)
-        button1.setOnClickListener{
-            val intent= Intent(requireContext(), ActivitySearch::class.java)
-            startActivity(intent)
-        }
-
-
-
-        val button2 = view.findViewById<Button>(R.id.share_car)
-        button2.setOnClickListener{
-            val intent= Intent(requireContext(), ActivityCars::class.java)
-            startActivity(intent)
-        }
-
-        val button3 = view.findViewById<Button>(R.id.b1)
-        button3.setOnClickListener{
-            val intent= Intent(requireContext(), ActivityPrize::class.java)
-            startActivity(intent)
-        }
-
-        val button5 = view.findViewById<Button>(R.id.b2)
-        button5.setOnClickListener{
-            val intent= Intent(requireContext(), ActivityGamification::class.java)
-            startActivity(intent)
-        }
-
-
-
-
-        val presentText = view.findViewById<TextView>(R.id.toPresent)
-        presentText.setOnClickListener {
-            val intent = Intent(requireContext(), ActivityGamification::class.java)
-            startActivity(intent)
-        }
-        val pointsText = view.findViewById<TextView>(R.id.toPoints)
-        pointsText.setOnClickListener{
-            val intent= Intent(requireContext(), ActivityPrize::class.java)
-            startActivity(intent)
-        }
-        val button4 = view.findViewById<Button>(R.id.b_thirdLine)
-        button4.setOnClickListener{
-            val intent= Intent(requireContext(), ActivityLeaderboard::class.java)
-            startActivity(intent)
-        }
-
         val prefs = requireContext().getSharedPreferences(R.string.shared_preferences.toString(), 0)
         val str_username=prefs.getString("username","")
-        if(str_username=="SweElena"){
-            view.third_part_title.text="Sei il 1° in classifica!"
+        if(str_username=="elena"){ //persona che ha sempre ruota
+            val view = inflater.inflate(R.layout.fragment_home, null)
+
+
+            val button1 = view.findViewById<Button>(R.id.find_car)
+            button1.setOnClickListener{
+                val intent= Intent(requireContext(), ActivitySearch::class.java)
+                startActivity(intent)
+            }
+
+
+
+            val button2 = view.findViewById<Button>(R.id.share_car)
+            button2.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityCars::class.java)
+                startActivity(intent)
+            }
+
+            val button3 = view.findViewById<Button>(R.id.b1)
+            button3.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityPrize::class.java)
+                startActivity(intent)
+            }
+
+            val button5 = view.findViewById<Button>(R.id.b2)
+            button5.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityGamification::class.java)
+
+                startActivity(intent)
+            }
+
+
+
+
+
+
+
+            val presentText = view.findViewById<TextView>(R.id.toPresent)
+            presentText.setOnClickListener {
+                val intent = Intent(requireContext(), ActivityGamification::class.java)
+
+                startActivity(intent)
+            }
+            val pointsText = view.findViewById<TextView>(R.id.toPoints)
+            pointsText.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityPrize::class.java)
+
+                startActivity(intent)
+            }
+            val button4 = view.findViewById<Button>(R.id.b_thirdLine)
+            button4.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityLeaderboard::class.java)
+                startActivity(intent)
+            }
+
+
+            if(str_username=="SweElena"){
+                view.third_part_title.text="Sei il 1° in classifica!"
+            }
+            if(str_username=="SweCyber13"){
+                view.third_part_title.text="Sei il 14° in classifica!"
+            }
+
+
+
+            return view
         }
-        if(str_username=="SweCyber13"){
-            view.third_part_title.text="Sei il 14° in classifica!"
+
+        else{//persona che può avere o meno ruota
+
+
+            if(FirstAccess.fa==true){ //persona primo accesso ha ruota
+                val view = inflater.inflate(R.layout.fragment_home, null)
+
+
+                val button1 = view.findViewById<Button>(R.id.find_car)
+                button1.setOnClickListener{
+                    val intent= Intent(requireContext(), ActivitySearch::class.java)
+                    startActivity(intent)
+                }
+
+
+
+                val button2 = view.findViewById<Button>(R.id.share_car)
+                button2.setOnClickListener{
+                    val intent= Intent(requireContext(), ActivityCars::class.java)
+                    startActivity(intent)
+                }
+
+                val button3 = view.findViewById<Button>(R.id.b1)
+                button3.setOnClickListener{
+                    val intent= Intent(requireContext(), ActivityPrize::class.java)
+                    startActivity(intent)
+                }
+
+                val button5 = view.findViewById<Button>(R.id.b2)
+                button5.setOnClickListener{
+                    val intent= Intent(requireContext(), ActivityGamification::class.java)
+                    FirstAccess.setFalse() //setto a false per il prox accesso
+                    startActivity(intent)
+                }
+
+                if(button5.isPressed){FirstAccess.setFalse()} //setto a false per il prox accesso
+
+
+
+
+
+                val presentText = view.findViewById<TextView>(R.id.toPresent)
+                presentText.setOnClickListener {
+                    val intent = Intent(requireContext(), ActivityGamification::class.java)
+                    FirstAccess.setFalse()
+                    startActivity(intent)
+                }
+                val pointsText = view.findViewById<TextView>(R.id.toPoints)
+                pointsText.setOnClickListener{
+                    val intent= Intent(requireContext(), ActivityPrize::class.java)
+
+                    startActivity(intent)
+                }
+                val button4 = view.findViewById<Button>(R.id.b_thirdLine)
+                button4.setOnClickListener{
+                    val intent= Intent(requireContext(), ActivityLeaderboard::class.java)
+                    startActivity(intent)
+                }
+
+
+                if(str_username=="SweElena"){
+                    view.third_part_title.text="Sei il 1° in classifica!"
+                }
+                if(str_username=="SweCyber13"){
+                    view.third_part_title.text="Sei il 14° in classifica!"
+                }
+
+                FirstAccess.setFalse()
+
+                return view
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            val view = inflater.inflate(R.layout.fragment_home2, null)
+
+
+            val button1 = view.findViewById<Button>(R.id.find_car)
+            button1.setOnClickListener{
+                val intent= Intent(requireContext(), ActivitySearch::class.java)
+                startActivity(intent)
+            }
+
+
+
+            val button2 = view.findViewById<Button>(R.id.share_car)
+            button2.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityCars::class.java)
+                startActivity(intent)
+            }
+
+            val button3 = view.findViewById<Button>(R.id.b1)
+            button3.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityPrize::class.java)
+                startActivity(intent)
+            }
+
+
+
+
+
+
+
+            val pointsText = view.findViewById<TextView>(R.id.toPoints)
+            pointsText.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityPrize::class.java)
+                startActivity(intent)
+            }
+            val button4 = view.findViewById<Button>(R.id.b_thirdLine)
+            button4.setOnClickListener{
+                val intent= Intent(requireContext(), ActivityLeaderboard::class.java)
+                startActivity(intent)
+            }
+
+        //    val prefs = requireContext().getSharedPreferences(R.string.shared_preferences.toString(), 0)
+        //    val str_username=prefs.getString("username","")
+        //    if(str_username=="SweElena"){
+        //        view.third_part_title.text="Sei il 1° in classifica!"
+        //    }
+        //    if(str_username=="SweCyber13"){
+        //        view.third_part_title.text="Sei il 14° in classifica!"
+        //    }
+
+
+            return view
+
         }
 
 
-        return view
+
 
 
 
